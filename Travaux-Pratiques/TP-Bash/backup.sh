b68db2f735bf0bc5db91a38471fddd57e92d5add
Faycal Z <faycal.seo@gmail.com>
2026-02-04 11:27:36 +0100

    TP Bash : Etape 1.2 Los et Vérifications

--- Travaux-Pratiques/TP-Bash/backup.sh
+++ Travaux-Pratiques/TP-Bash/backup.sh
@@ -1,22 +1,56 @@
 #!/bin/bash
 
-#On récupère le répertoire à sauvegarder
+#Configuration et ajout du fichier LOGFILE
 SOURCE=$1
 DESTINATION="/backup"
+LOGFILE="/var/log/backup.log"
+
+#Message log
+log_message() {
+    # Affiche la date, le message, et l'écrit aussi dans le fichier log
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a "$LOGFILE"
+}
+
+#Vérification de l'argument et message d'erreur
+if [ -z "$1" ]; then
+    echo "Erreur : Précisez un dossier."
+    exit 1
+fi
+
+#Ajout de la création du fichier log et vérification
+mkdir -p "$DESTINATION"
+touch "$LOGFILE" 2>/dev/null
 
 #Format de la date et nom de l'archive
 DATE=$(date +%Y%m%d_%H%M%S)
 NOM_ARCHIVE="backup_$DATE.tar.gz"
 
-#Création du dossier de destination
-mkdir -p "$DESTINATION"
+#Vérification de l'existence du dossier source
+if [ ! -d "$SOURCE" ]; then
+    log_message "ERREUR : Le dossier source '$SOURCE' n'existe pas !"
+    exit 1
+fi
+
+#Vérification de l'espace de disque
+TAILLE_SOURCE=$(du -s "$SOURCE" | awk '{print $1}')
+ESPACE_DISPO=$(df "$DESTINATION" | tail -1 | awk '{print $4}')
+if [ "$TAILLE_SOURCE" -gt "$ESPACE_DISPO" ]; then
+    log_message "ERREUR : Espace disque insuffisant dans $DESTINATION."
+    exit 1
+fi
+
 
 #Création de l'archive 
+log_message "Démarrage de la sauvegarde de $SOURCE..."
 tar -czf "$DESTINATION/$NOM_ARCHIVE" "$SOURCE" 2>/dev/null
 
-#Affichage du message de confirmation et de la taille
-echo "Sauvegarde réussie dans : $DESTINATION/$NOM_ARCHIVE"
-du -h "$DESTINATION/$NOM_ARCHIVE"
+#Vérification et affichage du message
+if [ $? -eq 0 ]; then
+    log_message "SUCCÈS : Archive créée ($DESTINATION/$NOM_ARCHIVE)"
+else
+    log_message "ERREUR : Échec de la compression."
+    exit 1
+fi
 
 
 
