d9d2c759363a9eb600d39f3ac62c9b725c99c02a
Faycal Z <faycal.seo@gmail.com>
2026-02-04 12:45:12 +0100

    TP Bash: Etape 2.2. Moniteur avec couleurs

--- Travaux-Pratiques/TP-Bash/monitor.sh
+++ Travaux-Pratiques/TP-Bash/monitor.sh
@@ -1,3 +1,24 @@
+#Ajout des couleurs
+VERT="\e[32m"
+JAUNE="\e[33m"
+ROUGE="\e[31m"
+RESET="\e[0m"
+
+#Fonction couleurs en fonction du pourcentage
+afficher_couleur() {
+    local VALEUR=$1
+    
+    local ENTIER=$(echo "$VALEUR" | awk '{print int($1)}')
+
+    if [ "$ENTIER" -ge 85 ]; then
+        echo -e "${ROUGE}${VALEUR}%${RESET}"   
+    elif [ "$ENTIER" -ge 70 ]; then
+        echo -e "${JAUNE}${VALEUR}%${RESET}"   
+    else
+        echo -e "${VERT}${VALEUR}%${RESET}"   
+    fi
+}
+
 #Titre du script
 echo "==========================="
 echo "         MONITOR           "
@@ -14,13 +35,15 @@ echo "Uptime    : $(uptime)"
 
 #Utilisation CPU
 CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
-echo "CPU Usage : $CPU_USAGE%"
+COLOR_CPU=$(afficher_couleur "$CPU_USAGE")
+echo -e "CPU Usage : $COLOR_CPU"
 
 #Utilisation mémoire
 MEM_TOTAL=$(free -h | grep Mem | awk '{print $2}')
 MEM_USED=$(free -h | grep Mem | awk '{print $3}')
 MEM_PERCENT=$(free | grep Mem | awk '{printf("%.2f", $3/$2 * 100.0)}')
-echo "Mémoire   : $MEM_USED / $MEM_TOTAL ($MEM_PERCENT%)"
+COLOR_MEM=$(afficher_couleur "$MEM_PERCENT")
+echo -e "Mémoire   : $MEM_USED / $MEM_TOTAL ($COLOR_MEM)"
 
 #Utilisation de chaque partition
 df -h | grep -vE '^Filesystem|tmpfs|cdrom|udev|loop' | awk '{ print $5 " " $1 }'
